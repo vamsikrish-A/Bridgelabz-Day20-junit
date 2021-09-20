1a84fbb5860f8cfa283af55a56aaecc116e92a9a
vamsikrish-A <krishnavamsi165@gmail.com>
2021-09-20 17:36:42 +0530

    welcome message

--- src/co/bridgelabz/junit/UserRegistration.java
+++ src/co/bridgelabz/junit/UserRegistration.java
@@ -1,5 +1,11 @@
 package co.bridgelabz.junit;
 
+/*
+* @author : Vamsi Krishna A
+*
+* @purpose: junit test for user Registration
+*
+* @date: 20/09/2021 */
 public class UserRegistration {
     public static void main(String[] args) {
         System.out.println("Welcome to JUnit Test Cases.");
