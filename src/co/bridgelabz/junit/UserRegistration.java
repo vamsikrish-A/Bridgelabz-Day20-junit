a22eb861f433957179f89fdd790c7a7da9e5e337
vamsikrish-A <krishnavamsi165@gmail.com>
2021-09-20 17:41:37 +0530

    validating FirstName

--- src/co/bridgelabz/junit/UserRegistration.java
+++ src/co/bridgelabz/junit/UserRegistration.java
@@ -1,5 +1,7 @@
 package co.bridgelabz.junit;
 
+import java.util.regex.Pattern;
+
 /*
 * @author : Vamsi Krishna A
 *
@@ -7,7 +9,19 @@ package co.bridgelabz.junit;
 *
 * @date: 20/09/2021 */
 public class UserRegistration {
+
+    public void validFirstName() {
+        //uSing Quantifiers for first name
+        System.out.println("Valid Firstname ");
+        System.out.println(Pattern.matches("[A-Z]{1}[a-z]{3}+","Vams"));  //True , first name starts with cap and has minimum 3 characters.
+        System.out.println(Pattern.matches("[A-Z]{1}[a-z]{3}+","Vamsi")); //false, has more characters.
+        System.out.println(Pattern.matches("[A-Z]{1}[a-z]{3}+","vams"));  //false, has no cap and more characters.
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to JUnit Test Cases.");
+        //Methods.
+        UserRegistration userRegistration = new UserRegistration();
+        userRegistration.validFirstName();
+
     }
 }
